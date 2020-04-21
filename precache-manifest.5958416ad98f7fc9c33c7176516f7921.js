7c239c5cae33cd9945aa0851cb60cdec40813da3
gafargits <rajigafaronline@gmail.com>
2020-04-21 13:18:56 +0100

    Updates

---
+++ precache-manifest.5958416ad98f7fc9c33c7176516f7921.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81f16878f676ed416ebb574374dd154",
+    "url": "/GyfeHome/index.html"
+  },
+  {
+    "revision": "ad906d6deff9aa3c1523",
+    "url": "/GyfeHome/static/css/main.c803e85d.chunk.css"
+  },
+  {
+    "revision": "6fcc2dbe7f8e0d9f0b90",
+    "url": "/GyfeHome/static/js/2.535f324c.chunk.js"
+  },
+  {
+    "revision": "ad906d6deff9aa3c1523",
+    "url": "/GyfeHome/static/js/main.7e7e7a38.chunk.js"
+  },
+  {
+    "revision": "6f0b3a64e8327f57ec35",
+    "url": "/GyfeHome/static/js/runtime~main.01e118fb.js"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/GyfeHome/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/GyfeHome/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "273448677c04eb0dd7037df1f6ea57cd",
+    "url": "/GyfeHome/static/media/gafar.27344867.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/GyfeHome/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/GyfeHome/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/GyfeHome/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/GyfeHome/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/GyfeHome/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "0458ce71d49a3adc3c69401a4c09b7a2",
+    "url": "/GyfeHome/static/media/starryNight.0458ce71.jpg"
+  }
+]);
